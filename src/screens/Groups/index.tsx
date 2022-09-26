3220597b0e2d981af9244a34c05d75ca98a01701
Moniky Monteiro <monikymonteiro.ci@gmail.com>
2022-09-26 15:09:09 -0300

    teams-react-native

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,77 @@
+import { useState, useCallback } from 'react';
+import { FlatList } from 'react-native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
+
+import { Container } from './styles';
+import { groupsGetAll } from '@storage/group/groupsGetAll';
+
+export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('new');
+
+  }
+
+  async function fetchGroups(){
+    try {
+      const data = await groupsGetAll();
+      setGroups(data);
+
+    } catch(error){
+      console.log(error);
+
+    }
+
+  }
+
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group })
+  }
+
+  useFocusEffect(useCallback(() => {
+    console.log("useFoccusEffect executou")
+    fetchGroups();
+  }, []));
+
+  return (
+    <Container>
+      <Header />
+      <Highlight 
+       title="Turmas"
+       subtitle="Jogue com a sua turma"
+      
+      />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+        <GroupCard 
+          title={item} 
+          onPress={() => handleOpenGroup(item)}
+         />
+  )}
+     contentContainerStyle={groups.length === 0 && {flex: 1}}
+     ListEmptyComponent={() => (
+     <ListEmpty message="Que tal cadastrar a primeira turma?"
+     />
+  )}
+  />
+
+   <Button
+    title="Criar nova turma" 
+    onPress={handleNewGroup}
+    
+    />
+    </Container>
+  );
+}
